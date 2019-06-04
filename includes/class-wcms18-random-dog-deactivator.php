7912cbc876437a346f6cb6db0259b63ddebbf9a1
Johan Nordström <johan@digitalvillage.se>
2019-06-04 14:52:12 +0200

    WPPB, totally unmodified. I promise!. LOL.

---
+++ includes/class-wcms18-random-dog-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       thehiveresistance.com
+ * @since      1.0.0
+ *
+ * @package    Wcms18_Random_Dog
+ * @subpackage Wcms18_Random_Dog/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wcms18_Random_Dog
+ * @subpackage Wcms18_Random_Dog/includes
+ * @author     Johan Nordström <jn@thehiveresistance.com>
+ */
+class Wcms18_Random_Dog_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
